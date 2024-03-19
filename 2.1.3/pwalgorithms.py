05a716306e7d329151709aa878f02f5df6ceb771
SantiagocDaSilva <santiago.dasilva@vtsd.com>
2024-03-19 09:25:57 -0400

    3/19/24

--- 2.1.3/pwalgorithms.py
+++ 2.1.3/pwalgorithms.py
@@ -2,26 +2,33 @@
 
 # get words from password dictionary file
 def get_dictionary():
-  words = []
-  dictionary_file = open("dictionary.txt")
-  for line in dictionary_file:
-    # store word, omitting trailing new-line
-    words.append(line[:-1])
-  dictionary_file.close()
-  return words
+    words = []
+    dictionary_file = open("dictionary.txt")
+    for line in dictionary_file:
+        # store word, omitting trailing new-line
+        words.append(line[:-1])
+    dictionary_file.close()
+    return words
+
 
 # analyze a one-word password
 def one_word(password):
-  words = get_dictionary()
-  guesses = 0
-  # get each word from the dictionary file
-  for w in words:
-    guesses += 1
-    if (w == password):
-      return True, guesses
-  return False, guesses
+    words = get_dictionary()
+    guesses = 0
+    # get each word from the dictionary file
+    for w in words:
+        guesses += 1
+        if (w == password):
+            return True, guesses
+    return False, guesses
+
 
 def two_words(password):
-  words = get_dictionary()
-  guesses = 0
-
+    words = get_dictionary()
+    guesses = 0
+    for w in words:
+        for w2 in words:
+            guesses += 1
+            if w + w2 == password:
+                return True, guesses
+        return False, guesses
