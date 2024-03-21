76fd5c20dffbce2f8e15e09c0110c5db47d48eb9
SantiagocDaSilva <santiago.dasilva@vtsd.com>
2024-03-21 07:57:03 -0400

    3/21/24

--- 2.1.4/main2.1.4.py
+++ 2.1.4/main2.1.4.py
@@ -9,22 +9,19 @@ root.wm_geometry("200x200")
 # create empty frame
 frame_login = tk.Frame(root)
 frame_login.grid()
-tk.Label(frame_login,text="Username:",font="Courier")
-tk.Label(frame_login,text="Password:",font="Courier")
+tk.Label(frame_login, text="Username:", font="Courier")
+tk.Label(frame_login, text="Password:", font="Courier")
+
+btn_login = tk.Button(frame_login, text='Login')
 
 lbl_username = tk.Label(frame_login, text='Username:')
 lbl_username.pack()
-
-ent_username = tk.Entry(frame_login, bd=3)
-ent_username.pack(pady=50)
-ent_username.pack(padx=50)
+ent_password = tk.Entry(frame_login, bd=3)
+ent_password.pack(pady=5)
 
 lbl_password = tk.Label(frame_login, text='Password:')
 lbl_password.pack()
-
 ent_password = tk.Entry(frame_login, bd=3)
-ent_password.pack(pady=50)
-ent_password.pack(padx=50)
-
+ent_password.pack(pady=5)
 
 root.mainloop()
